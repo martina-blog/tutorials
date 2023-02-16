862af8ac5180d5f7f582725b987f55eca4643fb3
Missing Parenthesis <53077720+missingparenthesis@users.noreply.github.com>
2023-02-16 08:47:42 +0100

    Add echo and print tutorial

---
+++ echo_and_print/index.php
@@ -0,0 +1,17 @@
+<?php
+// echo statement
+echo "This string is printed by using echo! \n";
+$string = "Hello World";
+echo "Display ", "value ", "from ", "variable: ", $string, "\n";
+echo "Display numbers: ", 62432, "\n";
+echo("Displaying string using parenthesis. \n");
+echo("hello"), (" world! \n");
+
+//print statement
+print "This string is printed by using print! \n";
+
+$string = "Hello World";
+print "Display " . "value " . "from " . "variable: ". $string . "\n";
+print "Display numbers: ". 62432 . "\n";
+print "Displaying string using parenthesis. \n";
+print "hello world! \n";
